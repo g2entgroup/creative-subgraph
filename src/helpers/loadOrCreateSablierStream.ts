7ffdb8933bcaf3de411e0584aa30553e4ad9e65a
G2 <bracy.gawain2014@gmail.com>
2021-05-09 14:26:12 -0400

    inital Subgraph
    inital subgraph commit
    
    Signed-off-by: G2 <bracy.gawain2014@gmail.com>

--- src/helpers/loadOrCreateSablierStream.ts
+++
@@ -1,9 +0,0 @@
-import { SablierStream } from "../../generated/schema"
-
-export function loadOrCreateSablierStream(id : string) : SablierStream{
-    let stream = SablierStream.load(id)
-    if(!stream){
-        stream = new SablierStream(id)
-    }
-    return stream as SablierStream 
-}
